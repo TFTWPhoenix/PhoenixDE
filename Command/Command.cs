58d8ca28738c508e25ab2aaa1a82fcd456c802ff
TFTWPhoenix <chucktcg12@gmail.com>
2021-02-04 21:56:15 -0600

    Fix a formatting goof

--- Command/Command.cs
+++ Command/Command.cs
@@ -4,19 +4,19 @@ namespace PhoenixDE {
     public class Command {
         public static void exec(String cmd) {
             if(cmd == "bash") {
-                    System.Diagnostics.Process proc = System.Diagnostics.Process.Start("/bin/bash");
-                    proc.WaitForExit();
-                } else if (cmd == "help") {
-                    ScreenUtil.printTitle("Commands");
-                    Console.WriteLine("help, bash, exit, zsh (requires zsh)");
-                    Console.WriteLine(ScreenUtil.fullline);
+                System.Diagnostics.Process proc = System.Diagnostics.Process.Start("/bin/bash");
+                proc.WaitForExit();
+            } else if (cmd == "help") {
+                ScreenUtil.printTitle("Commands");
+                Console.WriteLine("help, bash, exit, zsh (requires zsh)");
+                Console.WriteLine(ScreenUtil.fullline);
                     
-                } else if (cmd == "exit") {
-                    Environment.Exit(0);
-                } else if (cmd == "zsh") {
-                    System.Diagnostics.Process proc = System.Diagnostics.Process.Start("/bin/zsh");
-                    proc.WaitForExit();
-                }
+            } else if (cmd == "exit") {
+                Environment.Exit(0);
+            } else if (cmd == "zsh") {
+                System.Diagnostics.Process proc = System.Diagnostics.Process.Start("/bin/zsh");
+                proc.WaitForExit();
+            }
         }
     }
 }
